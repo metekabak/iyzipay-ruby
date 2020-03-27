7f68be24b504bf4604450cad213c949ef2685b8a
MehmetCelik4 <mehmetcelik4@gmail.com>
2020-03-27 19:05:25 +0300

    fix retvieve http method

--- lib/iyzipay/model/v2/checkout_form.rb
+++ lib/iyzipay/model/v2/checkout_form.rb
@@ -5,15 +5,16 @@ module Iyzipay
         RESOURCE = '/v2/subscription/checkoutform'
 
         def retrieve(request, options)
-          data = request.to_json
-          header = get_http_header(options, RESOURCE, data)
-          HttpClient.post_even_on_error(base_url(options, request[:token]), header, data)
+          header = get_http_header(options, RESOURCE)
+          HttpClient.get_even_on_error(base_url(options, request[:token]), header)
         end
 
         private
 
         def base_url(options, token)
+          Rails.logger.debug("-------------------------- #{options.base_url}#{RESOURCE}/#{token}")
           "#{options.base_url}#{RESOURCE}/#{token}"
+          
         end
       end
     end
